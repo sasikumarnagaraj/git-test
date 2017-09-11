34de7d14d47f0a9f5fbd10d6e5d413fa9a2cb1b4
sasikumarnagaraj <sasi.nagarajan@wipro.com>
2017-09-11 08:53:50 +0530

    new
    new line

--- EC2.java
+++ EC2.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
+      //New line
       System.out.println("Hello, World");
    }
 }
